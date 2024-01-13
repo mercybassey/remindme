3616d0e9dacb4dbfbba664f9d74d73b6b9a98755
mercybassey <udohmercy911@gmail.com>
2024-01-13 19:47:11 +0100

    Converted task description to lowercase

--- crud/add.py
+++ crud/add.py
@@ -21,6 +21,8 @@ def add(task, time, lead):
 
         current_time = datetime.now().strftime("%H:%M")
 
+        task_lower = task.lower()
+
         if not is_valid_lead_time(datetime.strptime(current_time, "%H:%M"), datetime.strptime(time, "%H:%M"), int(lead)):
             print("\033[91m✘ Invalid lead time. Lead time must be a positive duration from the current time to the scheduled time\033[0m")
             return
@@ -28,14 +30,14 @@ def add(task, time, lead):
         with sqlite3.connect(DB_FILE) as db:
             cursor = db.cursor()
             
-            cursor.execute("SELECT 1 FROM tasks WHERE description = ?", (task,))
+            cursor.execute("SELECT 1 FROM tasks WHERE description = ?", (task_lower,))
             exists = cursor.fetchone()
 
             if exists:
-                print(f"\033[91m✘ Task with description '{task}' already exists. Only tasks with distinct descriptions are allowed")
+                print(f"\033[91m✘ Task with description '{task_lower}' already exists. Only tasks with distinct descriptions are allowed")
                 return
 
-            cursor.execute('INSERT INTO tasks (description, scheduled_time, lead_time) VALUES (?, ?, ?)', (task, time, lead))
+            cursor.execute('INSERT INTO tasks (description, scheduled_time, lead_time) VALUES (?, ?, ?)', (task_lower, time, lead))
             db.commit()
 
         print(f"\033[92m✔ Task '{task}' added successfully")
